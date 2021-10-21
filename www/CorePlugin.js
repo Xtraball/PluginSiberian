85a58781c1b4208529b67ec18c42cb9a71dac343
Anders <anders@xtraball.com>
2021-10-21 17:21:05 +0200

    feat: update, rename sib plugin.

---
+++ www/CorePlugin.js
@@ -0,0 +1,30 @@
+/**
+ * Core plugin
+ */
+var exec = require('cordova/exec');
+
+exports.requestTrackingAuthorization = function (callback, scope) {
+    this.exec('requestTrackingAuthorization', null, callback, scope);
+};
+
+exports.createCallbackFn = function (callbackFn, scope) {
+    if (typeof callbackFn != 'function')
+        return;
+
+    return function () {
+        callbackFn.apply(scope || this, arguments);
+    };
+};
+
+exports.exec = function (action, args, callback, scope) {
+    var fn = this.createCallbackFn(callback, scope),
+        params = [];
+
+    if (Array.isArray(args)) {
+        params = args;
+    } else if (args) {
+        params.push(args);
+    }
+
+    exec(fn, null, 'CorePlugin', action, params);
+};
